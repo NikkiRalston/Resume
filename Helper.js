b6f4601068f0971642a5a4237e28e706df00dab9
NikkiRalston <94995862+NikkiRalston@users.noreply.github.com>
2023-01-29 19:02:36 -0700

    Add files via upload

---
+++ Helper.js
@@ -0,0 +1,9 @@
+function displayText(){
+    document.getElementById("myText").innerHTML = "Here will be some of my information or something else.";
+}
+function displayText2(htmlElement){
+    document.getElementById(htmlElement).innerHTML = "Under Construction.";
+}
+function displayText3(theId, displayText){
+    document.getElementById(theId).innerHTML = displayText;
+}
